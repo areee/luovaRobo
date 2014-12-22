c02400336967e16cb2a19cd4f9b7302ae9ba267a
Arttu Ylhävuori <areee@users.noreply.github.com>
2014-12-22 23:09:15 +0200

    mainia päivitetty

--- projekti/src/paaohjelma/Main.java
+++ projekti/src/paaohjelma/Main.java
@@ -5,14 +5,19 @@ import lejos.nxt.*;
 public class Main {
 
 	public static void main(String[] args) {
-
+		//Testiohjelma, jolla tutustun NXT:n ja Eclipsen "vuorovaikutukseen"
+		//Teen tähän pohjaan myös lopullisen työni
+		
 		System.out.println("Hei maailma! \nT. Arttu");
 
 		if (Button.LEFT.isPressed()) {
 			System.out.println("Vasenta näppäintä painettu.");
 		} else if (Button.RIGHT.isPressed()) {
-			System.out.println();
+			System.out.println("Oikeaa näppäintä painettu.");
+		} else if (Button.ENTER.isPressed()) {
+			System.out.println("Enteriä painettu.");
+		} else {
+			Button.waitForPress(); //tarkoitus olisi, että returnia painamalla lopettaa, ei toimi vielä oikein
 		}
-		Button.waitForPress();
 	}
 }
