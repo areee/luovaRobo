6fe0e76dc6c48463ad4019168376e94a910bb399
Arttu Ylhävuori <areee@users.noreply.github.com>
2014-12-23 23:46:27 +0200

    main.javaa päivitetty opettelutarkoituksessa

--- projekti/src/paaohjelma/Main.java
+++ projekti/src/paaohjelma/Main.java
@@ -1,23 +1,33 @@
 package paaohjelma;
 
+import javax.microedition.lcdui.Screen;
+
 import lejos.nxt.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		//Testiohjelma, jolla tutustun NXT:n ja Eclipsen "vuorovaikutukseen"
-		//Teen tähän pohjaan myös lopullisen työni
-		
-		System.out.println("Hei maailma! \nT. Arttu");
+		// Testiohjelma, jolla tutustun NXT:n ja Eclipsen "vuorovaikutukseen"
+		// Teen tähän pohjaan myös lopullisen työni
 
-		if (Button.LEFT.isPressed()) {
-			System.out.println("Vasenta näppäintä painettu.");
-		} else if (Button.RIGHT.isPressed()) {
-			System.out.println("Oikeaa näppäintä painettu.");
-		} else if (Button.ENTER.isPressed()) {
-			System.out.println("Enteriä painettu.");
-		} else {
-			Button.waitForPress(); //tarkoitus olisi, että returnia painamalla lopettaa, ei toimi vielä oikein
+		System.out.println("Paina jotakin" + "\nrobon painikkeista.");
+
+		while (true) {
+			int aika = Button.waitForPress(5000);
+			System.out.println(aika);
+			if (aika == 2) {
+				System.out.println("Vasenta nappainta painettu.");
+			} else if (aika == 4) {
+				System.out.println("Oikeaa nappainta painettu.");
+			} else if (aika == 1) {
+				System.out.println("Enteria painettu.");
+			} else if (aika == 8) {
+				System.out.println("Escapea painettu.");
+			} else if (aika == 0) {
+				System.out.println(aika);
+				break;
+			}
 		}
+
 	}
 }
