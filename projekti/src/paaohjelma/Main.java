d5860150498526aea02b57aa13339ea833487efd
Arttu Ylhävuori <areee@users.noreply.github.com>
2014-12-25 22:58:37 +0200

    main.java päivitetty: robon piirtotyön kehittämistä

--- projekti/src/paaohjelma/Main.java
+++ projekti/src/paaohjelma/Main.java
@@ -1,43 +1,44 @@
 package paaohjelma;
 
-import lejos.addon.gps.SimpleGPS;
 import lejos.nxt.*;
-import lejos.util.PilotProps;
 import lejos.robotics.navigation.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		// Moottoreiden toiminnan testailua:
-		NXTRegulatedMotor vasenPyora = Motor.A;
-		vasenPyora.setSpeed(150);
-
-		NXTRegulatedMotor oikeaPyora = Motor.C;
-		oikeaPyora.setSpeed(150);
-
 		NXTRegulatedMotor kynamoottori = Motor.B;
 		kynamoottori.setSpeed(15);
 
-//		vasenPyora.rotateTo(-150);
-//
-//		oikeaPyora.rotateTo(-150);
+		MoveController pilot = new DifferentialPilot(5.6f, 9.0f, Motor.A,
+				Motor.C);
+		pilot.setTravelSpeed(5);
 
-//		kynamoottori.rotate(90);
-//		kynamoottori.rotate(-90);
+		// NavPathController controller = new NavPathController(pilot);
+		
+		System.out.println("Hei, olen luova robotti!");
+		Sound.playNote(Sound.XYLOPHONE, 523, 500);
+		Sound.playNote(Sound.XYLOPHONE, 659, 500);
+		Sound.playNote(Sound.XYLOPHONE, 784, 500);
+		Sound.playNote(Sound.XYLOPHONE, 1047, 500);
+		
+		kynamoottori.rotate(45);
+		
+		System.out.println("Piirretaan...");
+		
+		pilot.travel(-20);
+		pilot.travel(20);
+		
+		System.out.println("Valmis!");
+		Sound.playNote(Sound.XYLOPHONE, 523, 500);
+		Sound.playNote(Sound.XYLOPHONE, 659, 500);
+		Sound.playNote(Sound.XYLOPHONE, 784, 500);
+		Sound.playNote(Sound.XYLOPHONE, 1047, 500);
+		kynamoottori.rotate(-45);
 
-		// Ajoneuvon luominen (testailua myöskin):
-		// Kesken...
-		MoveController pilot = new DifferentialPilot(5.6f, 9.0f, Motor.A, Motor.C);
-
-		NavPathController controller = new NavPathController(pilot);
-		
-		pilot.travel(-50);
-		
-		kynamoottori.rotate(90);
-		kynamoottori.rotate(-90);
-		
-		pilot.travel(50);
+		// ArcRotateMoveController controller2 = new DifferentialPilot(5.6f,
+		// 9.0f, Motor.A, Motor.C);
+		// controller2.arc(5, 360);
 
 	}
 }
